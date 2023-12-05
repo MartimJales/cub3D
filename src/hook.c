4703fb58c4846129e6702ad5cdd833de1b5e9a9e
MartimJales <m.jales@campus.fct.unl.pt>
2023-12-05 17:09:24 +0000

    Refactor key handling functions in hook.c

--- src/hook.c
+++ src/hook.c
@@ -6,21 +6,39 @@
 /*   By: mjales <mjales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 19:36:18 by mjales            #+#    #+#             */
-/*   Updated: 2023/11/09 10:02:40 by mjales           ###   ########.fr       */
+/*   Updated: 2023/12/05 16:16:10 by mjales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/cub3D.h"
 
-int	key_hook(int keycode, t_var *v)
+void handle_exit(int keycode)
 {
-	(void)v;
-	// printf("keycode = %d\n", keycode);
 	if (keycode == ESC_KEY_M || keycode == CROSS)
 	{
 		exit_program(vars());
 	}
-	else if (keycode == KEY_RIGHT_M)
+}
+
+void handle_movement(int keycode)
+{
+	if(keycode == KEY_UP_M)
+	{
+		// vars()->player->y -= 5;
+		vars()->player->y += vars()->player->deltaY;
+		vars()->player->x += vars()->player->deltaX;
+	}
+	else if (keycode == KEY_DOWN_M)
+	{
+		// vars()->player->y += 20;
+		vars()->player->y -= vars()->player->deltaY;
+		vars()->player->x -= vars()->player->deltaX;
+	}
+}
+
+void handle_rotation(int keycode)
+{
+	if (keycode == KEY_RIGHT_M)
 	{
 		// vars()->player->x += 5;
 		vars()->player->angle += 0.1;
@@ -37,18 +55,14 @@ int	key_hook(int keycode, t_var *v)
 		vars()->player->deltaX = cos(vars()->player->angle)*5;
 		vars()->player->deltaY = sin(vars()->player->angle)*5;
 	}
-	else if(keycode == KEY_UP_M)
-	{
-		// vars()->player->y -= 5;
-		vars()->player->y += vars()->player->deltaY;
-		vars()->player->x += vars()->player->deltaX;
-	}
-	else if (keycode == KEY_DOWN_M)
-	{
-		// vars()->player->y += 20;
-		vars()->player->y -= vars()->player->deltaY;
-		vars()->player->x -= vars()->player->deltaX;
-	}
+}
+
+int key_hook(int keycode, t_var *v)
+{
+	(void)v;
+    handle_exit(keycode);
+    handle_rotation(keycode);
+    handle_movement(keycode);
 	mlx_clear_window(vars()->win->mlx_ptr, vars()->win->win_ptr);
 	draw_map();
 	draw_player(vars()->player->img, playerSize, gen_trgb(0, 255, 255, 0), vars()->player->x, vars()->player->y);
@@ -56,15 +70,7 @@ int	key_hook(int keycode, t_var *v)
 	drawRays2D(*vars()->win);
 	mlx_put_image_to_window(vars()->win->mlx_ptr, vars()->win->win_ptr, vars()->player->img.img_ptr, vars()->player->x, vars()->player->y);
 	return (0);
-}
 
-int	mouse_hook(int keycode)
-{
-	// if (keycode == SCROLL_UP)
-	// 	zoom_in(vars()());
-	// else if (keycode == SCROLL_DOWN)
-	// 	zoom_out(vars()());
-	return (keycode);
 }
 
 int	exit_program(t_var *v)

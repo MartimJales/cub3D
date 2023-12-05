f40df27baef9047d30dd1b10e7ac4133bde35811
MartimJales <m.jales@campus.fct.unl.pt>
2023-12-05 17:09:00 +0000

    Remove unused function convert_range()

--- src/mouse.c
+++ src/mouse.c
@@ -6,14 +6,9 @@
 /*   By: mjales <mjales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 15:45:41 by mjales            #+#    #+#             */
-/*   Updated: 2023/10/17 11:44:36 by mjales           ###   ########.fr       */
+/*   Updated: 2023/12/05 16:17:15 by mjales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/cub3D.h"
-
-double	convert_range(double old_value, int max_value)
-{
-	return (((old_value + old_value) / max_value) - 1.0f);
-}
 

c6a2176592a4f91efc06c49ce2a3f2a0f220f652
MartimJales <m.jales@campus.fct.unl.pt>
2023-12-19 15:58:44 +0000

    Update parser norminette

--- src/parser.c
+++ src/parser.c
@@ -6,57 +6,74 @@
 /*   By: mjales <mjales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:04:27 by mjales            #+#    #+#             */
-/*   Updated: 2023/12/15 16:12:17 by mjales           ###   ########.fr       */
+/*   Updated: 2023/12/19 15:55:06 by mjales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/cub3D.h"
 
-char *ft_strncpy(char *dest, const char *src, size_t n) {
-    size_t i;
+char	*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++) {
-        dest[i] = src[i];
-    }
-    for ( ; i < n; i++) {
-        dest[i] = '\0';
-    }
+	i = 0;
 
-    return dest;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
 
-int char_to_int(char chr)
+
+int	char_to_int(char chr)
 {
 	return (chr - '0');
 }
 
-int check_string(char	*input_str)
+int	check_string(char	*input_str)
 {
-    int start_index = 0;
-    while (input_str[start_index] == ' ')
+	int	start_index;
+	int	end_index;
+
+	end_index = strlen(input_str) - 1;
+	start_index = 0;
+	while (input_str[start_index] == ' ')
 		start_index++;
-    int end_index = strlen(input_str) - 1;
-    while (end_index >= 0 && input_str[end_index] == ' ')
+
+	while (end_index >= 0 && input_str[end_index] == ' ')
 		end_index--;
-    if (input_str[start_index] == '1' && input_str[end_index] == '1')
-        return 0;
-    else
-        return 1;
+	if (input_str[start_index] == '1' && input_str[end_index] == '1')
+		return (0);
+	else
+		return (1);
 }
 
 
-int validate_string(char	*input_str)
+int	validate_string(char	*input_str)
 {
-	int index = 0;
-    while (input_str[index] != '\0')
-    {
-        char current_char = input_str[index];
-        if (!(char_to_int(current_char) == 0 ||  char_to_int(current_char) == 1 ||
-              current_char == 'N' || current_char == 'S' ||
-              current_char == 'E' || current_char == 'W' ||
-              current_char == ' '))
-            return 1;
-		if (current_char == 'N' || current_char == 'S' ||
+	int		index;
+	char	current_char;
+
+	index = 0;
+	while (input_str[index] != '\0')
+	{
+		current_char = input_str[index];
+		if (!(char_to_int(current_char) == 0 || \
+char_to_int(current_char) == 1 || \
+current_char == 'N' || current_char == 'S' || \
+current_char == 'E' || current_char == 'W' || \
+current_char == ' '))
+			return (1);
+		if (current_char == 'N' || current_char == 'S' || \
 			current_char == 'E' || current_char == 'W')
 		{
 			vars()->player->orientation = current_char;
@@ -64,135 +81,163 @@ int validate_string(char	*input_str)
 			vars()->player->starty = vars()->map_height;
 			current_char = '0';
 		}
-		if(current_char == ' ')
+		if (current_char == ' ')
 			current_char = '0';
 		vars()->map[vars()->map_height][index] = char_to_int(current_char);
-        index++;
-    }
-    return 0;
+		index++;
+	}
+	return (0);
 }
 
-int get_width(char *input_str) {
-    int start_index = 0;
-    int end_index = strlen(input_str) - 1;
-    while (start_index <= end_index && input_str[start_index] == ' ')
-        start_index++;
-    while (end_index >= start_index && input_str[end_index] == ' ')
-        end_index--;
-    int width = 0;
-    for (int i = start_index; i <= end_index; i++)
-        width++;
-    return width;
+int	get_width(char *input_str)
+{
+	int	start_index;
+	int	end_index;
+	int	width;
+	int	i;
+
+	start_index = 0;
+	end_index = strlen(input_str) - 1;
+	while (start_index <= end_index && input_str[start_index] == ' ')
+		start_index++;
+	while (end_index >= start_index && input_str[end_index] == ' ')
+		end_index--;
+	width = 0;
+	i = start_index;
+	while (i <= end_index)
+	{
+		width++;
+		i++;
+	}
+	return (width);
 }
 
 
-int convert_to_int(const char *start, const char *end) {
-    int length = end - start;
-    char number_str[4]; // Suficiente para armazenar um número até 255 e o caractere nulo
 
-    if (length >= 4 || length <= 0) {
-        printf("Erro: Segmento inválido.\n");
-        return -1;
-    }
+int	convert_to_int(const char *start, const char *end)
+{
+	int		length;
+	char	number_str[4];
 
-    ft_strncpy(number_str, start, length);
-    number_str[length] = '\0'; // Garantindo o terminador nulo
+	length = end - start;
 
-    return atoi(number_str);
+	if (length >= 4 || length <= 0)
+	{
+		printf("Erro: Segmento inválido.\n");
+		return (-1);
+	}
+
+	ft_strncpy(number_str, start, length);
+	number_str[length] = '\0';
+
+	return (atoi(number_str));
 }
 
-int process_string(const char *str) {
-    int red, green, blue;
-	red=0;
-	green=0;
-	blue=0;
-    const char *segment_start = str;
-    const char *current_char = str;
-    int segment_count = 0;
+int	process_string(const char *str)
+{
+	int			red;
+	int			green;
+	int			blue;
+	int 		segment_count;
+	const char	*segment_start;
+	const char	*current_char;
 
-    while (*current_char != '\0') {
-        if (*current_char == ',' || *(current_char + 1) == '\0') {
-            const char *segment_end = (*current_char == ',') ? current_char : current_char + 1;
-            int value = convert_to_int(segment_start, segment_end);
-            if (value == -1) {
-                return 0;
-            }
+	red = 0;
+	green = 0;
+	blue = 0;
+	segment_start = str;
+	current_char = str;
+	segment_count = 0;
 
-            if (segment_count == 0) red = value;
-            else if (segment_count == 1) green = value;
-            else if (segment_count == 2) blue = value;
+	while (*current_char != '\0') {
+		if (*current_char == ',' || *(current_char + 1) == '\0') {
+			const char *segment_end;
+			if (*current_char == ',') {
+				segment_end = current_char;
+			} else {
+				segment_end = current_char + 1;
+			}
 
-            segment_start = current_char + 1;
-            segment_count++;
-        }
-        current_char++;
-    }
+			int value = convert_to_int(segment_start, segment_end);
+			if (value == -1)
+				return (0);
+			if (segment_count == 0) red = value;
+			else if (segment_count == 1) green = value;
+			else if (segment_count == 2) blue = value;
 
-    if (segment_count != 3) {
-        printf("Erro: Número incorreto de segmentos.\n");
-        return 0;
-    }
+			segment_start = current_char + 1;
+			segment_count++;
+		}
+		current_char++;
+	}
+
+	if (segment_count != 3)
+	{
+		printf("Erro: Número incorreto de segmentos.\n");
+		return (0);
+	}
 	printf("red: %d\n", red);
 	printf("green: %d\n", green);
 	printf("blue: %d\n", blue);
-    return gen_trgb(255, red, green, blue);
+	return (gen_trgb(255, red, green, blue));
 }
+
 
 //auxiliares parsing
 char* ft_strrchr(const char *str, int c)
 {
-    char *ultima_ocorrencia = NULL;
+	char *ultima_ocorrencia = NULL;
 
-    while (*str != '\0') {
-        if (*str == c) {
-            ultima_ocorrencia = (char*)str;
-        }
-        str++;
-    }
+	while (*str != '\0') {
+		if (*str == c) {
+			ultima_ocorrencia = (char*)str;
+		}
+		str++;
+	}
 
-    return ultima_ocorrencia;
+	return ultima_ocorrencia;
 }
 
 int ft_strcmp(const char *s1, const char *s2)
 {
-    while (*s1 != '\0' && *s1 == *s2) {
-        s1++;
-        s2++;
-    }
+	while (*s1 != '\0' && *s1 == *s2) {
+		s1++;
+		s2++;
+	}
 
-    return *(unsigned char *)s1 - *(unsigned char *)s2;
+	return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
 int check_format(const char *nome_arquivo)
 {
-    const char *extensao = ft_strrchr(nome_arquivo, '.'); // Encontra a última ocorrência de '.'
+	const char *extensao = ft_strrchr(nome_arquivo, '.'); // Encontra a última ocorrência de '.'
 
-    if (extensao != NULL) {
-        if (ft_strcmp(extensao, ".cub") == 0) {
-            return 1; // Extensão correta
-        }
-    }
+	if (extensao != NULL) {
+		if (ft_strcmp(extensao, ".cub") == 0) {
+			return 1; // Extensão correta
+		}
+	}
 
-    return 0;// Extensão incorreta;
+	return 0;// Extensão incorreta;
 }
 
 char *get_next_line(int fd)
 {
-    int i = 0;
-    int rd = 0;
-    char character;
+	int i = 0;
+	int rd = 0;
+	char character;
 
-    char *buffer = malloc(100000); // check before the current 1 and then malloc
-    while ((rd = read(fd, &character, 1)) > 0)
-    {
-        buffer[i++] = character;
-        if (character == '\n')
-            break;
-    }
-    buffer[i-1] = '\0';
-    if (rd == -1 || i == 0 || (!buffer[i - 1] && !rd))
-        return (free(buffer), NULL);
-    return (buffer);
+	char *buffer = malloc(100000); // check before the current 1 and then malloc
+	while ((rd = read(fd, &character, 1)) > 0)
+	{
+		buffer[i++] = character;
+		if (character == '\n')
+			break;
+	}
+	buffer[i-1] = '\0';
+	if (rd == -1 || i == 0 || (!buffer[i - 1] && !rd))
+		return (free(buffer), NULL);
+	return (buffer);
 }
 
 void parseline(char *line)
@@ -265,13 +310,17 @@ void parser(char *filename)
 	printf("map_width: %d\n", vars()->map_width);
 }
 
-void  fill_image(t_img img, int color)
+void	fill_image(t_img img, int color)
 {
-	for (int i = 0; i < img.w; i++)
+	int i = 0;
+	while (i < img.w)
 	{
-		for (int j = 0; j < img.h; j++)
+		int j = 0;
+		while (j < img.h)
 		{
 			put_pixel_img(img, i, j, color);
+			j++;
 		}
+		i++;
 	}
 }
